77380a92d480a70444ef7c138cea263a72bc60a9
eduardaurbanovichi-png <eduardaurbanovichi@gmail.com>
2026-02-18 19:22:40 -0300

    Add files via upload

---
+++ aula_8/script.js
@@ -0,0 +1,20 @@
+
+
+let campoTexto = document.getElementById("texto");
+let campoCor = document.getElementById("cor");
+let resultado = document.getElementById("resultado");
+
+
+let botaoExibir = document.getElementById("btnExibir");
+let botaoLimpar = document.getElementById("btnLimpar");
+
+
+botaoExibir.addEventListener("click", function () {
+    resultado.innerText = campoTexto.value;
+    resultado.style.color = campoCor.value;
+});
+
+
+botaoLimpar.addEventListener("click", function () {
+    resultado.innerText = "";
+});
